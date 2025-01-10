20f5fd47f52ccbc7fe3a6ace21f624cba3766f0d
Vyktor Fellype <vyktorf07@gmail.com>
2025-01-10 01:25:41 -0300

    Implement blockchain integrity check and add additional block

--- Blockchain.js
+++ Blockchain.js
@@ -19,7 +19,18 @@ module.exports = class blockchain {
         console.log(`Next index incremented to: ${this.nextIndex}`);
     }
 
-    isValid(){
+    isValid(){ //Função para verificar a integridade da blockchain
+        for(let i= this.blocks.length - 1; i > 0; i--){ // Loop para percorrer os blocos da blockchain 
+            const currentBlock = this.blocks[i]; // Bloco atual
+            const previousBlock = this.blocks[i - 1]; // Bloco anterior
+
+            if(currentBlock.hash !== currentBlock.generateHash() 
+                || currentBlock.previousHash !== previousBlock.hash 
+                || currentBlock.index !== previousBlock.index + 1){ // Verifica se o hash do bloco atual é diferente do hash gerado
+                return false; // Retorna falso
+            }
+        }
+        return true //Se passou pela verificação tran quilo, returna true
         
     }
 }
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const Blockchain = require('./Blockchain'); // Importando a classe Blockchain
 
 const blockchain = new Blockchain(); // Criando uma nova instância da classe Blockchain
+
 blockchain.addBlock({from: 'Vyktor', to: 'Fellype', amount: 35}); // Adicionando um bloco à blockchain
+blockchain.addBlock({from: 'Pedro', to: 'André', amount: 5}); // Adicionando um bloco à blockchain
+
 
 console.log(blockchain); // Exibindo a blockchain no console
+console.log(blockchain.isValid()); // Verificando a integridade da blockchain
